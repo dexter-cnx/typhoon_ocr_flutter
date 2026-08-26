58eb9dabfb3c380dcf0747e7eff08d3df5bffabe
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-26 11:26:08 +0700

    test: accept JSON charset on minimum SDK

--- test/opentyphoon_cloud_provider_test.dart
+++ test/opentyphoon_cloud_provider_test.dart
@@ -61,7 +61,7 @@ void main() {
       'https://example.test/v1/chat/completions',
     );
     expect(capturedRequest.headers['Authorization'], 'Bearer test-key');
-    expect(capturedRequest.headers['Content-Type'], 'application/json');
+    expect(capturedRequest.headers['Content-Type'], startsWith('application/json'));
     expect(capturedBody['model'], 'typhoon-ocr');
 
     final messages = capturedBody['messages'] as List<dynamic>;
